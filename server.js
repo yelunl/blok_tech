b9d3435b156af69cfcad13c7f64320ace6a7a6a7
Yeliz U <yeliz.unlu@hva.nl>
2023-02-16 15:33:10 +0100

    express routes created

--- server.js
+++ server.js
@@ -3,8 +3,22 @@ const express = require('express');
 const app = express();
 const port = 5500;
 
+app.use(express.static('./static'));
+
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+  res.send('<h1>Hello World</h1><img src="./images/cloud_cake.jpeg" width="600px">');
+})
+
+app.get('/inloggen', (req, res) => {
+  res.send('dit is een inlogpagina');
+})
+
+app.get('/registreren', (req, res) => {
+  res.send('dit is een registratiepagina');
+})
+
+app.get('/account-pagina/:user', (req, res) => {
+  res.send(`dit is een accountpagina ${req.params.user}`);
 })
 
 app.listen(port, () => {
