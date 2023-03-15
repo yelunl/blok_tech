dc11944b7dd3fa4e8f492d42485c6a6a7cc2e1ed
Yeliz U <yeliz.unlu@hva.nl>
2023-03-15 01:08:21 +0100

    slider created

--- static/scripts/script.js
+++ static/scripts/script.js
@@ -1,6 +1,28 @@
+const slider = document.querySelector('div[class="slider"]');
+let currentPosition = '';
+
+slider.addEventListener('scroll', () => {
+    currentPosition = slider.scrollLeft;
+});
+
+window.addEventListener('click', (event) => {
+    if (currentPosition < 544 || currentPosition > 0) {
+        const sliderButtons = event.target.classList;
+        if (sliderButtons.contains('fa-circle-right')) {
+            currentPosition += 275;
+        } else if (sliderButtons.contains('fa-circle-left')) {
+            currentPosition -= 275;
+        }
+        slider.scrollLeft = currentPosition;
+    }
+
+});
+
+
+
+
 const longitudeInput = document.querySelector('#longitude');
 const latitudeInput = document.querySelector('#latitude');
-
 
 const succes = (position) => {
     longitudeInput.value = position.coords.longitude;
